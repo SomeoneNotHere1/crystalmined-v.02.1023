3c38969a7feaa81329df6efe401e917afc184552
SomeoneNotHere1 <98906856+SomeoneNotHere1@users.noreply.github.com>
2023-01-05 11:37:15 -0600

    Update conveyor.js

--- conveyor.js
+++ conveyor.js
@@ -5,7 +5,7 @@ const conveyor={
 			if(field[i+1].texture=='convRight.png' || field[i+1].texture=='convUp.png' || field[i+1].texture=='movything.png'){
 				let nextConv=field[i+1]
 				if(nextConv.texture != null && nextConv.overlayTexture == null){			
-					if(conv.cooldown>=(40/*-gameInfo.convSpeed*/)){
+					if(conv.cooldown>=(40-gameInfo.convSpeed)){
 						conv.cooldown=0
 						nextConv.overlayTexture=conv.overlayTexture
 						conv.overlayTexture=null
@@ -18,7 +18,7 @@ const conveyor={
 			if(field[i+1].texture=='upgraderblue.png'){
 				let nextConv=field[i+2]
 				if(nextConv.texture != null && nextConv.overlayTexture == null){			
-					if(conv.cooldown>=(40/*-gameInfo.convSpeed*/)){
+					if(conv.cooldown>=(40-gameInfo.convSpeed)){
 						conv.cooldown=0
 						if(nextConv.texture=='convRight.png' || nextConv.texture=='convUp.png' || nextConv.texture=='movything.png'){
 							if(conv.overlayTexture=='item1.png'){
@@ -35,7 +35,7 @@ const conveyor={
 			if(field[i+1].texture=='upgraderred.png'){
 				let nextConv=field[i+2]
 				if(nextConv.texture != null && nextConv.overlayTexture == null){			
-					if(conv.cooldown>=(40/*-gameInfo.convSpeed*/)){
+					if(conv.cooldown>=(40-gameInfo.convSpeed)){
 						conv.cooldown=0
 						if(nextConv.texture=='convRight.png' || nextConv.texture=='convUp.png' || nextConv.texture=='movything.png'){
 							if(conv.overlayTexture=='blue.png'){
@@ -52,7 +52,7 @@ const conveyor={
 			if(field[i+1].texture=='upgraderpurple.png'){
 				let nextConv=field[i+2]
 				if(nextConv.texture != null && nextConv.overlayTexture == null){			
-					if(conv.cooldown>=(40/*-gameInfo.convSpeed*/)){
+					if(conv.cooldown>=(40-gameInfo.convSpeed)){
 						conv.cooldown=0
 						if(nextConv.texture=='convRight.png' || nextConv.texture=='convUp.png' || nextConv.texture=='movything.png'){
 							if(conv.overlayTexture=='red.png'){
@@ -69,7 +69,7 @@ const conveyor={
 			if(field[i+1].texture=='upgraderpurple.png'){
 				let nextConv=field[i+2]
 				if(nextConv.texture != null && nextConv.overlayTexture == null){			
-					if(conv.cooldown>=(40/*-gameInfo.convSpeed*/)){
+					if(conv.cooldown>=(40-gameInfo.convSpeed)){
 						conv.cooldown=0
 						if(nextConv.texture=='convRight.png' || nextConv.texture=='convUp.png' || nextConv.texture=='movything.png'){
 							if(conv.overlayTexture=='red.png'){
@@ -86,7 +86,7 @@ const conveyor={
 			if(field[i+1].texture=='upgraderblack.png'){
 				let nextConv=field[i+2]
 				if(nextConv.texture != null && nextConv.overlayTexture == null){			
-					if(conv.cooldown>=(40/*-gameInfo.convSpeed*/)){
+					if(conv.cooldown>=(40-gameInfo.convSpeed)){
 						conv.cooldown=0
 						if(nextConv.texture=='convRight.png' || nextConv.texture=='convUp.png' || nextConv.texture=='movything.png'){
 							if(conv.overlayTexture=='purple.png'){
@@ -105,7 +105,7 @@ const conveyor={
 			if(field[i-1].texture=='convLeft.png' || field[i-1].texture=='convUp.png' || field[i-1].texture=='movything.png'){
 			let nextConv=field[i-1]
 				if(nextConv.texture != null && nextConv.overlayTexture == null){			
-					if(conv.cooldown>=(40/*-gameInfo.convSpeed*/)){
+					if(conv.cooldown>=(40-gameInfo.convSpeed)){
 						conv.cooldown=0
 						nextConv.overlayTexture=conv.overlayTexture
 						conv.overlayTexture=null
@@ -120,7 +120,7 @@ const conveyor={
 			if(field[i-20].texture=='convUp.png' || field[i-20].texture=='convLeft.png' || field[i-20].texture=='convRight.png'){
 			let nextConv=field[i-20]
 				if(nextConv.texture != null && nextConv.overlayTexture == null){			
-					if(conv.cooldown>=(40/*-gameInfo.convSpeed*/)){
+					if(conv.cooldown>=(40-gameInfo.convSpeed)){
 						conv.cooldown=0
 						nextConv.overlayTexture=conv.overlayTexture
 						conv.overlayTexture=null
@@ -135,7 +135,7 @@ const conveyor={
 			if(field[i+20].texture=='movything.png' || field[i+20].texture=='convLeft.png' || field[i+20].texture=='convRight.png'){
 			let nextConv=field[i+20]
 				if(nextConv.texture != null && nextConv.overlayTexture == null){			
-					if(conv.cooldown>=(40/*-gameInfo.convSpeed*/)){
+					if(conv.cooldown>=(40-gameInfo.convSpeed)){
 						conv.cooldown=0
 						nextConv.overlayTexture=conv.overlayTexture
 						conv.overlayTexture=null
